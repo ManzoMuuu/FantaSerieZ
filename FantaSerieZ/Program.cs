bc8dda83221d3adc66736bc8ba681b5325933b11
ManzoMuuu <li7edark@gmail.com>
2024-02-27 15:30:03 +0100

    Refactoring

--- FantaSerieZ/Program.cs
+++ FantaSerieZ/Program.cs
@@ -33,37 +33,8 @@ app.UseAuthorization(); // Assicurati di aggiungere questo se utilizzi l'autenti
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapControllers(); // Aggiunto per mappare i controller
+    _ = endpoints.MapControllers(); // Aggiunto per mappare i controller
 });
 
 app.Run();
 
-/* using Microsoft.EntityFrameworkCore;
-using WebApiEntityFrameworkDockerSqlServer.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddDbContext<SalesContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FantaSerieZTest")));
-
-var app = builder.Build();
-
-using(var scope = app.Services.CreateScope())
-{
-    var salesContext = scope.ServiceProvider.GetRequiredService<SalesContext>();
-    salesContext.Database.EnsureCreated();
-    salesContext.Seed();
-}
-
-// Configure the HTTP request pipeline.
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
- */
